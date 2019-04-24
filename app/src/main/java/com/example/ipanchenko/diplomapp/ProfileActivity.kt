96c1e0c736748cf401b3d95480bb612f31627cfe
i.panchenko <iliapanchenko@gmail.com>
2019-04-24 18:58:41 +0300

    Строка и кнопка поиска - отображается. Изменена страница пользователя (добавлена информация о пользователе, пост, и фотография с помощью библиотеки "Picasso")

--- app/src/main/java/com/example/ipanchenko/diplomapp/ProfileActivity.kt
+++ app/src/main/java/com/example/ipanchenko/diplomapp/ProfileActivity.kt
@@ -1,7 +1,11 @@
 package com.example.ipanchenko.diplomapp
 
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : BaseActivity(4) {
     private val TAG = "ProfileActivity"
@@ -11,5 +15,9 @@ class ProfileActivity : BaseActivity(4) {
         setContentView(R.layout.activity_profile)
         setupBottomNavigation()
         Log.d(TAG,"onCreate")
+
+
+        Picasso.get().load("https://pbs.twimg.com/profile_images/1120306723892400128/j6ZRWJ0q_400x400.jpg").into(profile_image)
+        Picasso.get().load("https://pbs.twimg.com/profile_images/1120306723892400128/j6ZRWJ0q_400x400.jpg").into(profile_image_tweet)
     }
 }
